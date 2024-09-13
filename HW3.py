791e2ec662af199e6ada7fa674551d957c33d55b
Xinying Hou <51358925+xinyinghou@users.noreply.github.com>
2024-09-13 16:54:32 -0400

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,150 @@
+# Your name: 
+# Your student id:
+# Your email:
+# Who or what you worked with on this homework (including generative AI like ChatGPT):
+# If you worked with generative AI also add a statement for how you used it.  
+# e.g.: 
+# Asked Chatgpt hints for debugging and suggesting the general sturcture of the code
+
+# create a Digital Book of Answers
+class DigitalBookofAnswers():
+
+    # create the constructor (__init__) method 
+    # ARGUMENTS: 
+    #       self: the current object
+    #       answers: a list of potential answers
+    # RETURNS: None
+    def __init__(self, answers):
+
+        pass
+
+
+    # Create the __str__ method
+    # ARGUMENTS: 
+    #       self: the curent object
+    # RETURNS: a string with all of the answers in the book_answer_list separated by dashes
+    #        If no questions have been asked, return an empty string
+    def __str__(self):
+
+        pass
+
+    # Creates the check_get_answer method
+    # ARGUMENTS:
+    #       self: the current object
+    #       question: the question the user wants to ask the digital book of answers
+    # RETURNS: a string with the answer
+    def check_get_answer(self, question):
+
+        pass
+
+    # Creates open_book method
+    # ARGUMENTS:
+    #   self: the current object
+    # RETURNS: None
+    def open_book(self):
+
+        pass
+
+
+    # Create the answer_log method
+    # ARGUMENTS: 
+    #       self: the curent object
+    # RETURNS: list with frequency information for all answers in a sorted order
+    #          if no answers have been given, return an empty list
+    def answer_log(self):
+
+        pass
+
+
+def test():
+    answers_list = ['Believe in Yourself', 'Stay Open to the Future', 'Enjoy It']
+    book = DigitalBookofAnswers(answers_list)
+
+    print("Test __init__:")
+    print(f"Answer History List: Expected: {[]}, Actual: {book.answered_list}")
+    print(f"Question History List: Expected: {[]}, Actual: {book.questions_asked_list}")
+    print(" ")
+
+    print("Test __str__:")
+    expected = "Belive in Yourself - Stay Open to the Future - Enjoy It"
+    print(f"Expected: {expected}, Actual: {str(book)}")
+    print(" ")
+
+
+    print("Testing return value of check_get_answer:")
+    res = book.check_get_answer('test question')
+    print(f"Expected: {str}, Actual: {type(res)}")
+    print(" ")
+
+    print("Testing check_get_answer")
+    book.book_answer_list = ['Go For It']
+    res = book.check_get_answer('test question 2')
+    print(f"Expected: {'Go For It'}, Actual: {res}")
+    print(" ")
+
+    print("Testing that check_get_answer adds answer index to answered_list:")
+    book.book_answer_list = ['Go For It']
+    book.answered_list = []
+    book.check_get_answer('test question 2')
+    expected = [0]
+    res = book.answered_list
+    print(f"Expected: {expected}, Actual: {res}")
+    print(" ")
+
+    print("Testing that check_get_answer does not add 'I've already answered this question' part to answered_list:")
+    book.book_answer_list = ['Believe In Yourself']
+    book.answered_list = [0]
+    book.questions_asked_list = ['test question 3']
+    book.check_get_answer('test question 3')
+    expected = [0, 0]
+    res = book.answered_list
+    print(f"Expected: {expected}, Actual: {res}")
+    print(" ")
+
+
+    print("Testing return value answer_log")
+    book.book_answer_list = ['Follow Your Inner Voice', 'Stay Positive', 'Go For It']
+    book.answered_list = [0, 0, 0, 1, 1, 2]
+    res = type(book.answer_log())
+    print(f"Expected: {list}, Actual: {res}")
+    print(" ")
+
+    print("Testing return value answer_log elements")
+    book.answered_list = [0, 0, 0, 1, 1, 2]
+    res = type(book.answer_log()[0])
+    print(f"Expected: {str}, Actual: {res}")
+    print(" ")
+
+    print("Testing answer_log")
+    book.answered_list = [0, 0, 0, 1, 1, 2]
+    res = book.answer_log()
+    expected = ['follow your inner voice - 3', 'stay positive - 2', 'go for it - 1']
+    print(f"Expected: {expected}, Actual: {res}")
+    print(" ")
+
+    print("Testing empty answer_log")
+    book.answered_list = []
+    res = book.answer_log()
+    expected = []
+    print(f"Expected: {expected}, Actual: {res}")
+    print(" ")
+
+
+# Extra Credit
+def my_test():
+    # Put your test code here
+
+    pass
+
+
+def main():
+    pass
+
+
+
+# Only run the main function if this file is being run (not imported)
+if __name__ == "__main__":
+    #main()
+    test() 
+    # my_test() #TODO: Uncomment if you do the extra credit
+    
